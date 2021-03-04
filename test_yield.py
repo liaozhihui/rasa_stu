e79dc4113f66e26e90279a1a83e4e208bd411dcc
lzh <1581627402@qq.com>
2021-03-04 20:25:08 +0800

    rasa

---
+++ test_yield.py
@@ -0,0 +1,42 @@
+
+def xiecheng(func):
+
+    def warpper():
+        result = func()
+        next(result)
+        return result
+    return warpper
+
+def yield_test():
+    vegetables = []
+    while True:
+        food = yield vegetables
+        if food is None:
+            break
+        vegetables.append(food)
+
+        print(f"elements in foodlist are:{vegetables}")
+
+
+@xiecheng
+def proxy_gen():
+    while True:
+        vegetables = yield from yield_test()
+        print(f"final elements in foodlist are{vegetables}")
+
+def main():
+    g = proxy_gen()
+
+    print(g.send(1))
+    print(g.send(2))
+    print(g.send(3))
+    g.send(None)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
